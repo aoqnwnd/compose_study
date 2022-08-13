a552346365ff530baba4f41a3beb9525d63ccddd
jacob <aoqnwnd@naver.com>
2022-08-13 18:57:37 +0900

    compose 리스트 만들기~

--- ComposeTutorial/app/src/main/java/com/work_guide/composetutorial/MainActivity.kt
+++ ComposeTutorial/app/src/main/java/com/work_guide/composetutorial/MainActivity.kt
@@ -1,19 +1,37 @@
 package com.work_guide.composetutorial
 
+import android.content.res.Configuration
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.work_guide.composetutorial.ui.theme.ComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(Message("Android", "Jetpack Compose"))
+            ComposeTutorialTheme {
+                Conversation(SampleData.conversationSample)
+            }
         }
     }
 }
@@ -30,8 +48,86 @@ data class Message(
 
 @Composable
 fun MessageCard(msg: Message) {
-    Column {
-        Text(text = msg.author)
-        Text(text = msg.body)
+    Row(modifier = Modifier.padding(8.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.icon_profile_base),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                // 이미지 크기
+                .size(40.dp)
+                // 이미지 모양을 동그랗게
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+        )
+
+        // 좌우 빈 공간 view -> width or height
+        Spacer(modifier = Modifier.width(8.dp))
+
+        // 이 변수에 메시지가 확장되었는지 여부를 추적합니다.
+        var isExpanded by remember { mutableStateOf(false) }
+        // surfaceColor 는 한 색에서 다른 색으로 점차 업데이트됩니다.
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
+        )
+
+        // 이 열을 클릭할 때 isExpanded 변수를 전환합니다.
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondary,
+                style = MaterialTheme.typography.subtitle2
+            )
+            // 상하 빈 공간
+            Spacer(modifier = Modifier.height(4.dp))
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    // 메시지가 확장되면 모든 내용이 표시됩니다. 그렇지 않으면 첫 번째 줄만 표시합니다.
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.body2
+                )
+            }
+        }
+    }
+}
+
+@Preview(
+    name = "Light Mode", showBackground = true
+)
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+
+@Composable
+fun PreviewMessageCard() {
+    ComposeTutorialTheme {
+        MessageCard(
+            msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
+        )
+    }
+}
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversation() {
+    ComposeTutorialTheme {
+        Conversation(SampleData.conversationSample)
     }
 }
