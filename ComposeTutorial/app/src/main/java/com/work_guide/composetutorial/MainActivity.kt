240cf00b028c3435362d9f16302c9c0e171b4a99
jacob <aoqnwnd@naver.com>
2022-08-13 17:58:17 +0900

    add ignore file and compose column text

--- ComposeTutorial/app/src/main/java/com/work_guide/composetutorial/MainActivity.kt
+++ ComposeTutorial/app/src/main/java/com/work_guide/composetutorial/MainActivity.kt
@@ -3,12 +3,9 @@ package com.work_guide.composetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.work_guide.composetutorial.ui.theme.ComposeTutorialTheme
 
@@ -16,28 +13,25 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeTutorialTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MessageCard(Message("Android", "Jetpack Compose"))
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MessageCard(name: String) {
+    Text(text = "hello $name!")
 }
 
-@Preview(showBackground = true)
+data class Message(
+    val author: String,
+    val body: String
+)
+
 @Composable
-fun DefaultPreview() {
-    ComposeTutorialTheme {
-        Greeting("Android")
+fun MessageCard(msg: Message) {
+    Column {
+        Text(text = msg.author)
+        Text(text = msg.body)
     }
 }
